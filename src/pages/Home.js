e1804d7b6f760f95fc8dcd1b9849f80065cdd9a6
Christopher Bengtsson <christopherbengtsson@ChristophersMBP.lan>
2019-08-20 00:27:04 +0200

    invites

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
-import { happy, add } from 'ionicons/icons';
+import { happy, add, mail, close, checkmarkCircleOutline, closeCircleOutline } from 'ionicons/icons';
 import {
   IonButtons,
   IonContent,
@@ -21,11 +21,28 @@ import {
   IonSkeletonText,
   IonLabel,
   IonModal,
-  IonInput
+  IonItemSliding,
+  IonItemOption,
+  IonItemOptions
 } from '@ionic/react';
 import CreateGameModal from '../components/CreateGameModal';
 
-const Home = ({ games, history }) => {
+const sceletonCard = (<IonCard key='1'>
+  <IonCardHeader>
+    <IonCardSubtitle>
+      <IonSkeletonText animated style={{ width: '10%' }} />
+    </IonCardSubtitle>
+    <IonCardTitle style={{ textAlign: 'center' }}>
+      <IonSkeletonText animated style={{ width: '20%%' }} />
+    </IonCardTitle>
+  </IonCardHeader>
+
+  <IonCardContent>
+    <IonSkeletonText animated style={{ width: '40%%' }} />
+  </IonCardContent>
+</IonCard>);
+
+const Home = ({ games, uid, history }) => {
 
   const [showModal, setShowModal] = useState(false);
 
@@ -57,43 +74,78 @@ const Home = ({ games, history }) => {
         <IonModal
           isOpen={showModal}
           onDidDismiss={() => setShowModal(false)}>
+          <IonItem style={{ width: '6em' }} onClick={() => setShowModal(false)}>
+            <IonIcon slot='start' icon={close}></IonIcon>
+          </IonItem>
           <CreateGameModal />
           <IonButton onClick={() => setShowModal(false)}>CREATE GAME</IonButton>
         </IonModal>
 
+        {games && games.map(invite => {
+          if (!invite.acceptedInvites.includes(uid))
+            return <IonItemSliding key={invite.id}>
+              <IonItem>
+                <IonIcon slot="start" icon={mail}></IonIcon>
+                <IonLabel>
+                  <h2>New Invite</h2>
+                  <p>From {invite.players.find(player =>
+                    player.uid === invite.admin)
+                    .displayName}</p>
+                </IonLabel>
+              </IonItem>
+              <IonItemOptions side='start'>
+                <IonItemOption
+                  expandable
+                  color='danger'
+                  onClick={() => console.log('Reject Invite')}
+                >
+                  <IonIcon slot="start" icon={closeCircleOutline}></IonIcon>
+                  Reject
+                  </IonItemOption>
+              </IonItemOptions>
+              <IonItemOptions side='end'>
+                <IonItemOption
+                  expandable
+                  color='success'
+                  onClick={() => console.log('Accept Invite')}
+                >
+                  <IonIcon slot="end" icon={checkmarkCircleOutline}></IonIcon>
+                  Accept
+                  </IonItemOption>
+              </IonItemOptions>
+            </IonItemSliding>
+        })}
+
         {games ?
           <IonList>
             {games.map(game => {
-              return <IonCard key={game.id}>
-                <IonCardHeader>
-                  <IonCardSubtitle>
-                    {game.status === 'pending' ? game.status : game.activePlayer}
-                  </IonCardSubtitle>
-                  <IonCardTitle style={{ textAlign: 'center' }}>{
-                    game.title}
-                  </IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                  {(new Date(game.lastUpdated)).toString()}
-                </IonCardContent>
-              </IonCard>
+              if (game.acceptedInvites.includes(uid)) {
+                // Status active or pending
+                return <IonCard
+                  key={game.id}
+                  // href='/game'
+                  onClick={() => { // TODO: Only for dev. Remove.
+                    console.log(game)
+                  }}
+                >
+                  <IonCardHeader>
+                    <IonCardSubtitle>
+                      {game.status === 'pending' ? game.status : game.activePlayer}
+                    </IonCardSubtitle>
+                    <IonCardTitle
+                      style={{
+                        textAlign: 'center'
+                      }}
+                    >{game.title}
+                    </IonCardTitle>
+                  </IonCardHeader>
+                  <IonCardContent>
+                    {(new Date(game.lastUpdated)).toString()}
+                  </IonCardContent>
+                </IonCard>
+              }
             })}
-          </IonList> :
-
-          <IonCard key='1'>
-            <IonCardHeader>
-              <IonCardSubtitle>
-                <IonSkeletonText animated style={{ width: '10%' }} />
-              </IonCardSubtitle>
-              <IonCardTitle style={{ textAlign: 'center' }}>
-                <IonSkeletonText animated style={{ width: '20%%' }} />
-              </IonCardTitle>
-            </IonCardHeader>
-
-            <IonCardContent>
-              <IonSkeletonText animated style={{ width: '40%%' }} />
-            </IonCardContent>
-          </IonCard>
+          </IonList> : sceletonCard
         }
       </IonContent>
     </>
