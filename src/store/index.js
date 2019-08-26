93dbd253c02c61daf1efe177a18d9c33ecd1c8c8
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2019-08-26 20:11:56 +0200

    window.devToolsExtension deprecated

--- src/store/index.js
+++ src/store/index.js
@@ -8,7 +8,7 @@ const store = createStore(
     initialState,
     compose(
         applyMiddleware(thunk),
-        window.devToolsExtension ? window.devToolsExtension() : f => f
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
